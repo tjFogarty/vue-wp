2dd46587a0bb7e73ded0db1f9982f8958b649dcd
T.J. Fogarty <fogarty.tj@gmail.com>
2017-03-22 21:17:34 +0000

    Reformats functions.php

--- wp-content/themes/teej/functions.php
+++ wp-content/themes/teej/functions.php
@@ -1,62 +1,64 @@
 <?php
 
-if ( ! class_exists( 'Timber' ) ) {
-	add_action( 'admin_notices', function() {
-		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php') ) . '</a></p></div>';
-	});
-	
-	add_filter('template_include', function($template) {
-		return get_stylesheet_directory() . '/static/no-timber.html';
-	});
-	
-	return;
+namespace Teej;
+
+use Timber\Timber;
+use Timber\Menu as TimberMenu;
+use Timber\Site as TimberSite;
+
+if (!class_exists('Timber')) {
+    add_action('admin_notices', function () {
+        echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url(admin_url('plugins.php#timber')) . '">' . esc_url(admin_url('plugins.php')) . '</a></p></div>';
+    });
+
+    add_filter('template_include', function ($template) {
+        return get_stylesheet_directory() . '/static/no-timber.html';
+    });
+
+    return;
 }
 
 Timber::$dirname = array('templates', 'views');
 
-class StarterSite extends TimberSite {
+class TeejSite extends TimberSite
+{
+    public function __construct()
+    {
+        add_theme_support('post-formats');
+        add_theme_support('post-thumbnails');
+        add_theme_support('menus');
+        add_theme_support('html5', array('comment-list', 'comment-form', 'search-form', 'gallery', 'caption'));
+        add_filter('timber_context', array($this, 'addToContext'));
+        add_filter('get_twig', array($this, 'addToTwig'));
+        add_action('init', array($this, 'registerPostTypes'));
+        add_action('init', array($this, 'registerTaxonomies'));
+        parent::__construct();
+    }
 
-	function __construct() {
-		add_theme_support( 'post-formats' );
-		add_theme_support( 'post-thumbnails' );
-		add_theme_support( 'menus' );
-		add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
-		add_filter( 'timber_context', array( $this, 'add_to_context' ) );
-		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
-		add_action( 'init', array( $this, 'register_post_types' ) );
-		add_action( 'init', array( $this, 'register_taxonomies' ) );
-		parent::__construct();
-	}
+    public function registerPostTypes()
+    {
+        //this is where you can register custom post types
+    }
 
-	function register_post_types() {
-		//this is where you can register custom post types
-	}
+    public function registerTaxonomies()
+    {
+        //this is where you can register custom taxonomies
+    }
 
-	function register_taxonomies() {
-		//this is where you can register custom taxonomies
-	}
+    public function addToContext($context)
+    {
+        $context['menu'] = new TimberMenu();
+        $context['site'] = $this;
+        return $context;
+    }
 
-	function add_to_context( $context ) {
-		$context['foo'] = 'bar';
-		$context['stuff'] = 'I am a value set in your functions.php file';
-		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
-		$context['menu'] = new TimberMenu();
-		$context['site'] = $this;
-		return $context;
-	}
-
-	function myfoo( $text ) {
-		$text .= ' bar!';
-		return $text;
-	}
-
-	function add_to_twig( $twig ) {
-		/* this is where you can add your own functions to twig */
-		$twig->addExtension( new Twig_Extension_StringLoader() );
-		$twig->addFilter('myfoo', new Twig_SimpleFilter('myfoo', array($this, 'myfoo')));
-		return $twig;
-	}
-
+    public function addToTwig($twig)
+    {
+        /* this is where you can add your own functions to twig */
+        // $twig->addExtension( new Twig_Extension_StringLoader() );
+        // $twig->addFilter('myfoo', new Twig_SimpleFilter('myfoo', array($this, 'myfoo')));
+        return $twig;
+    }
 }
 
-new StarterSite();
+new TeejSite();
