3b7e22096054707ffafda0cf1f2c1920f067b35b
T.J. Fogarty <fogarty.tj@gmail.com>
2017-03-31 13:21:28 +0100

    Adds better pjax support

--- wp-content/themes/teej/src/js/main.js
+++ wp-content/themes/teej/src/js/main.js
@@ -1,7 +1,19 @@
 import Utils from './utils';
 import Barba from 'barba.js';
+import * as analytics from './analytics/base.js';
 
 Utils.ready(() => {
+  analytics.init();
   Barba.Pjax.start();
   Barba.Prefetch.init();
+
+  // track page views when using pjax
+  Barba.Dispatcher.on('newPageReady', () => {
+    ga('send', 'pageview', window.location.pathname);
+  });
+
+  Barba.Dispatcher.on('transitionCompleted', () => {
+    Prism.highlightAll();
+  });
+
 });
