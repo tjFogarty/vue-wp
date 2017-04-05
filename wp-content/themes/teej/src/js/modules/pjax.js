329c5345b6bd4849d05c8d16ba731aff6c73ed56
T.J. Fogarty <fogarty.tj@gmail.com>
2017-04-05 09:11:42 +0100

    Removes some transitions until they're ironed out

--- wp-content/themes/teej/src/js/modules/pjax.js
+++ wp-content/themes/teej/src/js/modules/pjax.js
@@ -1,76 +1,31 @@
 import Utils from '../utils';
+import Barba from 'barba.js';
 
+/**
+ * This object is responsible for any PJAX-related functionality
+ * @type {Object}
+ */
 const Pjax = {
-  Barba: null,
-  anime: null,
-
+  /**
+   * Load all our dependencies
+   * when they're done, kick things off
+   * @return {void}
+   */
   init () {
-    Promise.all([
-      import('barba.js'),
-      import('animejs')
-    ]).then(([Barba, anime]) => {
-      this.Barba = Barba;
-      this.anime = anime;
+    Barba.Pjax.start();
+    Barba.Prefetch.init();
 
-      this.Barba.Pjax.start();
-      this.Barba.Prefetch.init();
-
-      this.addEvents();
-      this.setupTransitions();
-    });
+    this.addEvents();
   },
 
+  /**
+   * Any events we need to hook into
+   */
   addEvents () {
-    this.Barba.Dispatcher.on('newPageReady', () => {
+    Barba.Dispatcher.on('newPageReady', () => {
       ga('send', 'pageview', window.location.pathname);
       Utils.detectCodeHighlight();
     });
-  },
-
-  setupTransitions () {
-    let FadeTransition = this.Barba.BaseTransition.extend({
-      start: function () {
-        Promise
-          .all([this.newContainerLoading, this.fadeOut()])
-          .then(this.fadeIn.bind(this));
-      },
-
-      fadeOut: function () {
-        let animation = Pjax.anime({
-          targets: this.oldContainer,
-          opacity: 0,
-          translateY: 30
-        });
-
-        return animation.finish;
-      },
-
-      fadeIn: function () {
-        let el = this.newContainer;
-
-        this.oldContainer.style.display = 'none';
-
-        el.style.visibility = 'visible';
-        el.style.opacity = 0;
-        el.style.transform = 'translateY(20px)';
-
-        window.scroll(0, 0);
-
-        let animation = Pjax.anime({
-          targets: el,
-          opacity: 1,
-          translateY: 0
-        });
-
-        animation.complete = () => {
-          this.done();
-        };
-      }
-    });
-
-    this.Barba.Pjax.getTransition = function() {
-      return FadeTransition;
-    };
   }
 };
 
--- wp-content/themes/teej/src/js/utils.js
+++ wp-content/themes/teej/src/js/utils.js
@@ -1,16 +1,22 @@
 const Utils = {
   documentReady (fn = () => {}) {
-    if (document.readyState != 'loading'){
+    if (document.readyState != 'loading') {
       fn();
     } else {
       document.addEventListener('DOMContentLoaded', fn);
     }
   },
 
+  /**
+   * See if there's any code to be highlighted
+   * if so, load a library
+   * @return {void}
+   */
   detectCodeHighlight () {
     if (document.querySelector('pre')) {
       import('prismjs')
-        .then(Prism => Prism.highlightAll());
+        .then(Prism => Prism.highlightAll())
+        .catch(err => console.log(err));
     }
   }
 };
