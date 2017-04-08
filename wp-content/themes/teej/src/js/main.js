8ea34febf7ac1b2a48e61267fbd3052c420b9eb4
T.J. Fogarty <fogarty.tj@gmail.com>
2017-04-08 20:02:50 +0100

    Updates styles for header and footer

--- wp-content/themes/teej/src/js/main.js
+++ wp-content/themes/teej/src/js/main.js
@@ -1,10 +1,8 @@
 import Utils from './utils';
-import Pjax from './modules/pjax';
 
 import('./analytics/base')
   .then(analytics => analytics.init());
 
 Utils.documentReady(() => {
   Utils.detectCodeHighlight();
-  Pjax.init();
 });
